8065d235f099cc95265315ff3d0ce683702743d3
liutao <2652174750@qq.com>
2019-03-15 00:00:45 +0800

    liutao

--- javaSocket/SocketClient/src/com/socket/ClsMainClient.java
+++ javaSocket/SocketClient/src/com/socket/ClsMainClient.java
@@ -29,12 +29,18 @@ public class ClsMainClient {
 		};
 		
 		//c1.connect("127.0.0.1", 1234);
+		//创建键盘录入对象
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入名字：");
+		//字符串接收消息
 		String a=sc.nextLine();
+		//将此线程的名称更改为等于参数 a
 		c1.setName(a);
+		//建立线程链接
 		c1.connect("127.0.0.1", 1234);
+		//睡眠时间调用
 		delay();
+		//循环登陆，发送消息给服务器证明用户登陆成功
 		while (true) {
 			if (c1.isConnected()) {
 				c1.getTransceiver().send("0qzjk"+a);
@@ -42,11 +48,13 @@ public class ClsMainClient {
 			}
 		}
 		System.out.println("连上了");
+		//循环读取消息，发送消息
 		while (true) {
 			a=sc.nextLine();
 			c1.getTransceiver().send("2qzjk"+a);
 		}
 	}
+	//模拟登陆延迟，捕获线程在等待，睡眠或以其他方式占用时抛出，线程在活动之前或活动期间中断的报错
 	static void delay() {
 		try {
 			Thread.sleep(1000);
