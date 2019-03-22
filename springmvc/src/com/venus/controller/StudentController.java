0b54cbe7b3fd63a25b6287010c9d9606d4ed542c
learninginb <674954347@qq.com>
2019-03-22 22:54:52 +0800

    xuqi

--- springmvc/src/com/venus/controller/StudentController.java
+++ springmvc/src/com/venus/controller/StudentController.java
@@ -1,127 +1,125 @@
 package com.venus.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.venus.domain.DataGrid;
+import com.venus.domain.Pagination;
 import com.venus.domain.Student;
 import com.venus.service.StudentService;
-
-import utill.Page;
 
 @Controller
 @RequestMapping("")
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+	@Autowired
+	private StudentService studentService;
 
-    @RequestMapping("/addStudent")
-    public String addStudent(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/addStudent.do")
+	public String addStudent(HttpServletRequest request,
+			HttpServletResponse response) {
 
-        Student student = new Student();
+		Student student = new Student();
 
-        int studentID = Integer.parseInt(request.getParameter("id"));
-        int classID = Integer.parseInt(request.getParameter("classid"));
-        String name = request.getParameter("name");
-        String sex = request.getParameter("sex");
-        String nation = request.getParameter("nation");
-        Date birthday = null;
-        // String 类型按照 yyyy-MM-dd 的格式转换为 java.util.Date 类
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            birthday = simpleDateFormat.parse(request.getParameter("birthday"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+		int studentID = Integer.parseInt(request.getParameter("id"));
+		int classID = Integer.parseInt(request.getParameter("classid"));
+		String name = request.getParameter("name");
+		String sex = request.getParameter("sex");
+		String nation = request.getParameter("nation");
+		Date birthday = null;
+		// String 类型按照 yyyy-MM-dd 的格式转换为 java.util.Date 类
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			birthday = simpleDateFormat.parse(request.getParameter("birthday"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
-        student.setId(studentID);
-        student.setName(name);
-        student.setClassId(classID);
-        student.setSex(sex);
-        student.setBirthday(birthday);
-        student.setNation(nation);
+		student.setId(studentID);
+		student.setName(name);
+		student.setClassId(classID);
+		student.setSex(sex);
+		student.setBirthday(birthday);
+		student.setNation(nation);
 
-        studentService.addStudent(student);
+		studentService.addStudent(student);
 
-        return "redirect:student_list";
-    }
+		return "redirect:student_list";
+	}
 
-    @RequestMapping("/listStudent")
-    public String listStudent(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("toStudentList.do")
+	public ModelAndView toClassList(HttpServletRequest request,
+			HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView("student_list"); // html文件名
+		return mav;
+	}
 
-        // 获取分页参数
-        int start = 0;
-        int count = 10;
+	@RequestMapping(value = "StudentList.do")
+	@ResponseBody
+	public DataGrid ClassList(HttpServletRequest request,
+			HttpServletResponse response) {
+		DataGrid dataGrid = new DataGrid();
+		List<Student> list = this.studentService.selectAllStudent();
+		Pagination pagination = new Pagination();
+		pagination.setList(list);
+		dataGrid.setTotal(pagination.getTotalCount());
+		dataGrid.setRows(pagination.getList());
+		return dataGrid;
+	}
 
-        try {
-            start = Integer.parseInt(request.getParameter("page.start"));
-            count = Integer.parseInt(request.getParameter("page.count"));
-        } catch (Exception e) {
-        }
+	@RequestMapping("/deleteStudent.do")
+	public String deleteStudent(int id) {
+		studentService.deleteStudent(id);
+		return "redirect:student_list";
+	}
 
-        Page page = new Page(start, count);
+	@RequestMapping("/editStudent.do")
+	public ModelAndView editStudent(int id) {
+		ModelAndView mav = new ModelAndView("editStudent");
+		Student student = studentService.getStudent(id);
+		mav.addObject("student", student);
+		return mav;
+	}
 
-        List<Student> students = studentService.list(page.getStart(), page.getCount());
-        int total = studentService.getTotal();
-        page.setTotal(total);
+	@RequestMapping("/updateStudent.do")
+	public String updateStudent(HttpServletRequest request,
+			HttpServletResponse response) {
 
-        request.setAttribute("students", students);
-        request.setAttribute("page", page);
+		Student student = new Student();
 
-        return "student_list";
-    }
+		int studentID = Integer.parseInt(request.getParameter("id"));
+		int classID = Integer.parseInt(request.getParameter("classid"));
+		String name = request.getParameter("name");
+		String sex = request.getParameter("sex");
+		String nation = request.getParameter("nation");
 
-    @RequestMapping("/deleteStudent")
-    public String deleteStudent(int id) {
-        studentService.deleteStudent(id);
-        return "redirect:student_list";
-    }
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date birthday = null;
+		try {
+			birthday = simpleDateFormat.parse(request.getParameter("birthday"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
-    @RequestMapping("/editStudent")
-    public ModelAndView editStudent(int id) {
-        ModelAndView mav = new ModelAndView("editStudent");
-        Student student = studentService.getStudent(id);
-        mav.addObject("student", student);
-        return mav;
-    }
+		student.setId(studentID);
+		student.setClassId(classID);
+		student.setName(name);
+		student.setSex(sex);
+		student.setBirthday(birthday);
+		student.setNation(nation);
 
-    @RequestMapping("/updateStudent")
-    public String updateStudent(HttpServletRequest request, HttpServletResponse response) {
-
-        Student student = new Student();
-
-        int studentID = Integer.parseInt(request.getParameter("id"));
-        int classID = Integer.parseInt(request.getParameter("classid"));
-        String name = request.getParameter("name");
-        String sex = request.getParameter("sex");
-        String nation = request.getParameter("nation");
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date birthday = null;
-        try {
-            birthday = simpleDateFormat.parse(request.getParameter("birthday"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        student.setId(studentID);
-        student.setClassId(classID);
-        student.setName(name);
-        student.setSex(sex);
-        student.setBirthday(birthday);
-        student.setNation(nation);
-        
-
-        studentService.updateStudent(student);
-        return "redirect:student_list";
-    }
+		studentService.updateStudent(student);
+		return "redirect:student_list";
+	}
 }
