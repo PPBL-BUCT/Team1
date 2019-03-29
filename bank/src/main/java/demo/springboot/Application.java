9bd4bc05f0b17fed58d26a112d2607039a63e68e
Li-dacheng <1047425378@qq.com>
2019-03-29 19:58:49 +0800

    login2.0

--- bank/src/main/java/demo/springboot/Application.java
+++ bank/src/main/java/demo/springboot/Application.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by bysocket on 26/09/2017.
  */
 @SpringBootApplication
-public class WebApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
     
 }
