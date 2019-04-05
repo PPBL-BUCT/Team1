b3075f82e6de9b694ad597f8c4416b4e176a3723
guozixuan <1059342346@qq.com>
2019-04-05 22:03:56 +0800

    log

--- bank/src/main/java/demo/springboot/util/ReceiveBalance.java
+++ bank/src/main/java/demo/springboot/util/ReceiveBalance.java
@@ -4,27 +4,20 @@ import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
 
-public class ReceiveBalance{
-	
-	public String getBalance() {
-    String res = HttpConnection.doPost(Core.getUrl_GetBalance(),
-	GetBalance.shape("6218129087231776"));
+public class ReceiveBalance {
+	public static String receiveBalance() {
+	    String res = HttpConnection.doPost(Core.getUrl_GetBalance(),
+		GetBalance.shape("6218129087231776"));
 
 
-    System.out.println(res);
-	JSONObject jsonObject= JSONObject.parseObject(res);
-	Map<String,Object>returnJson=(Map<String,Object>)jsonObject;
-	
-	Map<String,Object>returnBody=(Map<String,Object>)returnJson.get("Body");
-	
-	System.out.println(returnBody.get("Balance"));
-	
-	return (String) returnBody.get("Balance");
-	}
-
-	
-	
+	    System.out.println(res);
+		JSONObject jsonObject= JSONObject.parseObject(res);
+		Map<String,Object>returnJson=(Map<String,Object>)jsonObject;
+		
+		Map<String,Object>returnBody=(Map<String,Object>)returnJson.get("Body");
+		
+		System.out.println(returnBody.get("Balance"));
+		
+		return (String) returnBody.get("Balance");
+		}
 }
-	
-
-
