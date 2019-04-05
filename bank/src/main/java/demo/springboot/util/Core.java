1265ae3f1ea2a38eff6b5c2a7368bfd7e0a28c26
guozixuan <1059342346@qq.com>
2019-04-05 10:58:23 +0800

    重传

--- bank/src/main/java/demo/springboot/util/Core.java
+++ bank/src/main/java/demo/springboot/util/Core.java
@@ -1,10 +1,28 @@
 package demo.springboot.util;
 
+
 public class Core {
 
-	public final static String IP = "1"; // 流类型-流
-	public final static String PORT = "1"; // 流类型-流
+	public final static String IP = "118.25.143.204"; // 流类型-流
+	public final static String PORT = "8080"; // 流类型-流
 
-	public final static String NAME = "1"; // 流类型-流
-	public static final String GET_BALANCE = "1";
+	public final static String NAME = "HostSimulator"; // 流类型-流
+    public static final String GET_BALANCE = "GetAccountBalance.d";
+    public static final String ACCOUNT_CHECK= "AccountPasswordValidate.do";
+	
+	public String getUrl_GetBalance() {
+		
+		String url_GetBalance= "{http://"+IP+":"+PORT+"/"+NAME+"/"+GET_BALANCE+"}";
+		 
+		return url_GetBalance;
+	}
+            	        
+    
+	public String getUrl_AccountCheck() {
+				
+		String url_AccountCheck="{http://"+IP+":"+PORT+"/"+NAME+"/"+ACCOUNT_CHECK+"}";
+	 
+		return url_AccountCheck;
+	}
+         
 }
